69acfef3756004d1edae6950200e84cfa0d32260
Hitoshi <hitoshi.gaia.galaxy@gmail.com>
2020-03-15 16:00:53 +0900

    hello_world

---
+++ lesson.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+/*
+func init() {
+	fmt.Println("Init")
+}
+*/
+
+func bazz(){
+	fmt.Println("Bass")
+}
+
+func main(){
+	//bazz()
+	fmt.Println("Hello world!", "TEST TEST")
+}
