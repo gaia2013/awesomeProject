fca241f411ce5e7af55dac35238591891821cdfd
Hitoshi <hitoshi.gaia.galaxy@gmail.com>
2020-03-20 17:09:28 +0900

    3-23

---
+++ lesson.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main()  {
+	f := 1.11
+	i := int(f)
+	fmt.Printf("%T %v\n", i, i)
+
+	s := []int{1,2,5,6,2,3,1}
+	fmt.Println(s[2:4])
+
+	m := map[string]int{
+		"Mike": 20,
+		"Nancy": 24,
+		"Messi": 30,
+	}
+	fmt.Printf("%T %v", m, m)
+}
