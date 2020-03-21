4a79e930377d5a335d32cbb4035804f6896fe907
Hitoshi <hitoshi.gaia.galaxy@gmail.com>
2020-03-21 17:20:57 +0900

    6-39

---
+++ lesson.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+type Vertex struct {
+	X, Y int
+}
+
+func (v Vertex) Area() int {
+	return v.X * v.Y
+}
+
+func (v *Vertex) Scale(i int) {
+	v.X = v.Y * i
+	v.Y = v.Y * i
+}
+func Area(v Vertex) int {
+	return v.X * v.Y
+}
+
+func main() {
+	v := Vertex{3, 4}
+	v.Scale(10)
+	fmt.Println(Area(v))
+	fmt.Println(v.Area()) // .で呼び出せるものをメソッド
+
+}
